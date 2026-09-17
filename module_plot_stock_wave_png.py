801269649b6974e4ffc5d3896e3b5d593b9028d9
luckboy <luckboy@localhost>
2026-09-18 01:11:41 +0800

    清理废弃脚本与大盘整体情况旧页面，绘图脚本改名module前缀，概览页导航对齐首页

--- module_plot_stock_wave_png.py
+++ module_plot_stock_wave_png.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from mysql_connection import get_mysql_connection, close_connection
+from module_mysql_connection import get_mysql_connection, close_connection
 
 plt.rcParams['font.sans-serif'] = ['Arial Unicode MS', 'SimHei', 'STHeiti']
 plt.rcParams['axes.unicode_minus'] = False
